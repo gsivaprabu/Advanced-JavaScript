39e3bdbce765b7d85c1d8e6b7d75f3c6b071ad83
Naveena Sivaprabu <naveenasivaprabu@gmail.com>
2016-12-26 17:42:59 +0530

    Hoisting Changes

--- 1 Scope/15 Hoisting/Readme.md
+++ 1 Scope/15 Hoisting/Readme.md
@@ -2,4 +2,8 @@
 
 - Conceptual model called hoisting.
 - Moving to the top thing is what we called hoisting.
+- recursion
+	- **A function called itself**
+- Mutual recursion
+	- two are more functions calling each others.
 -
--- 1 Scope/15 Hoisting/script.js
+++ 1 Scope/15 Hoisting/script.js
@@ -9,7 +9,7 @@ a;
 b;*/
 
 
-var a = b();
+/*var a = b();
 var c = d();
 
 a;
@@ -21,4 +21,50 @@ function b() {
 
 var d = function() {
     return b();
+}*/
+
+
+/*foo(); //"foo"
+
+var foo = 2;
+
+function foo(){
+		 console.log('bar');
 }
+function foo(){
+		 console.log('foos');
+}
+
+function foo(){
+		 console.log('foo');
+}
+
+console.log('foo => '+ foo);*/
+
+
+
+/*a(1);
+
+function a(foo) {
+    if (foo > 20)
+        return foo
+    return b(foo + 2)
+}
+
+function b(foo) {
+    return c(foo) + 1;
+}
+
+function c(foo) {
+    return c(foo * 2);
+}
+*/
+
+function foo(bar) {
+    if (bar) {
+        console.log(baz); //reference Error
+        let baz = bar;
+    }
+}
+
+foo("bar");
