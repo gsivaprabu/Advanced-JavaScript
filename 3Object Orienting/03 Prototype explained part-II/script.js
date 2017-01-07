4c3c1c4f24bde637f0fa743ddd0eaadc4de59be6
Naveena Sivaprabu <naveenasivaprabu@gmail.com>
2017-01-07 14:27:30 +0530

    explained part-II changes

--- 3Object Orienting/03 Prototype explained part-II/script.js
+++ 3Object Orienting/03 Prototype explained part-II/script.js
@@ -1,7 +1,6 @@
 function Foo(who) {
     this.me = who;
 }
-
 Foo.prototype.identify = function() {
     return "I am " + this.me;
 };
@@ -9,12 +8,25 @@ Foo.prototype.identify = function() {
 var a1 = new Foo("a1");
 var a2 = new Foo("a2");
 
-
 a2.speak = function() {
     alert("Hello, " + this.identify() + ".");
 };
 
-a1.__proto__ === Object.getProtoTypeOf(a1);
-a2.constructor === Foo;
-a1.__proto__ === a2.__proto__;
-a2.__proto__ === a2.constructor.prototype;
+/*console.log(a1.constructor === Foo);
+console.log(a1.constructor === a2.constructor);
+console.log(a1.__proto__ === Foo.prototype);
+console.log(a1.__proto__ === a2.__proto__);*/
+
+console.log('Object.getPrototypeOf("foo")', Object.getPrototypeOf("foo"));
+
+var proto = [];
+var obj = Object.create(proto);
+result = Object.getPrototypeOf(obj)
+console.log('result', result);
+
+console.log(Object.getPrototypeOf(obj) === proto);
+
+console.log(a1.__proto__ === Object.getPrototypeOf(a1));
+console.log(a2.constructor === Foo);
+console.log(a1.__proto__ === a2.__proto__);
+console.log(a2.__proto__ === a2.constructor.prototype);
